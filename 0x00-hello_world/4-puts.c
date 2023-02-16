9b4481be82e723856982e1d0bf7c3b8fac61ddd5
MegumiinUwU <youssefmohamed81222 @gmail.com>
2023-02-16 14:53:42 -0800

    0x00. C - Hello, World tasks details in README

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main() {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
     char x[]="\"Programming is like building a multilingual puzzle";
 
     puts(x);
