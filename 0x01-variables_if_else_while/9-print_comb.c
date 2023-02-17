d609cdc0d117564389c98cc73fa251a68f9140ea
MegumiinUwU <youssefmohamed81222 @gmail.com>
2023-02-17 14:56:09 -0800

    0x01. C - Variables, if, else, while task DETAILS IN README

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all possible combinations of single-digit numbers.
+ * followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+        int c[10] = {0,1,2,3,4,5,6,7,8,9};
+        int i;
+        for ( i = 0; i <10 ; ++i)
+        {
+
+                putchar(48 + c[i]);
+                if (c[i] != 9)
+                {
+                        putchar(',');
+                        putchar(' ');
+                }
+
+        }
+
+        putchar('\n');
+        return (0);
+}
